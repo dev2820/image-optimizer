797631998bf6f2a1087c11920bf9c647af0be885
dev2820 <second28200@naver.com>
2026-02-15 19:37:20 +0900

    chore: add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/image-optimizer/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
