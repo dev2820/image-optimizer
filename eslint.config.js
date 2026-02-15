3d5f2ff2c52eda517952fceb87a7a3540bbcc92c
dev2820 <second28200@naver.com>
2026-02-15 11:38:09 +0900

    refactor: fix shadcn lint error

--- eslint.config.js
+++ eslint.config.js
@@ -30,4 +30,10 @@ export default defineConfig([
       'simple-import-sort/exports': 'error',
     },
   },
+  {
+    files: ['src/components/ui/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  },
 ])
